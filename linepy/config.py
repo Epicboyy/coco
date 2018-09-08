6a9b7b3b36f1aa7f5279554f6d5dfc0a0de10768
Epicboyy <41615146+Epicboyy@users.noreply.github.com>
2018-09-08 20:09:25 +0800

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,10 +27,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '8.0.2'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.0.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'CoCo'
+    SYSTEM_NAME = '莫言'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
